79994f1e9e3f3e082fa31a4e20969c5742e9ce6c
EmanSaleh94 <eman.s.ibrahim94@gmail.com>
2024-10-13 11:46:06 +0400

    final-ui-changes

---
+++ src/app/payout/page.tsx
@@ -0,0 +1,136 @@
+import React from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import SelectGroupTwo from "@/components/SelectGroup/SelectGroupTwo";
+import CountrySelect from "@/components/payout/CountrySelect";
+import MerchantTable from "@/components/payout/MerchantTable";
+import { Package } from "@/types/package";
+import Card from "@/components/payout/Card";
+
+const packageData: Package[] = [
+  {
+    id: 1,
+    name: "Free package",
+    amount: 0.0,
+    requestTime: `Jan 13,2023`,
+    requester: "Paid",
+    payoutProvider: "Paymob",
+    daysDue: "3 days",
+  },
+  {
+    id: 2,
+    name: "Standard Package",
+    amount: 59.0,
+    requestTime: `Jan 13,2023`,
+    requester: "Paid",
+    payoutProvider: "Paymob",
+    daysDue: "3 days",
+  },
+  {
+    id: 3,
+    name: "Business Package",
+    amount: 99.0,
+    requestTime: `Jan 13,2023`,
+    requester: "Unpaid",
+    payoutProvider: "Paymob",
+    daysDue: "3 days",
+  },
+  {
+    id: 4,
+    name: "Standard Package",
+    amount: 59.0,
+    requestTime: `Jan 13,2023`,
+    requester: "Pending",
+    payoutProvider: "Paymob",
+    daysDue: "3 days",
+  },
+];
+
+const tableFields = [
+  "ID",
+  "Merchant name",
+  "Amount",
+  "Payout provider",
+  "Request time",
+  "Requester",
+  " Days due",
+  "Actions",
+];
+
+const Page = () => {
+  return (
+    <DefaultLayout>
+      <div className="drawer drawer-open flex h-lvh font-semibold text-bodydark2 dark:bg-boxdark">
+        <div role="tablist" className="tabs tabs-lifted m-auto mt-3">
+          <input
+            type="radio"
+            name="my_tabs_2"
+            role="tab"
+            className="tab"
+            aria-label="Pending Approval"
+            defaultChecked
+          />
+          <div
+            role="tabpanel"
+            className="tab-content rounded-box border-base-300 bg-base-100 p-6"
+          >
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <MerchantTable
+                fieldNames={tableFields}
+                tableValues={packageData}
+              />
+            </div>
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_2"
+            role="tab"
+            className="tab"
+            aria-label="Approval Hisroty"
+          />
+          <div
+            role="tabpanel"
+            className="tab-content rounded-box border-base-300 bg-base-100 p-6"
+          >
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <MerchantTable
+                fieldNames={tableFields}
+                tableValues={packageData}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="drawer-side">
+          <div className="menu min-h-full w-60 bg-black p-4 ">
+            <div>
+              <label className="= mb-3 block text-sm font-medium dark:text-white">
+                Find Merchant
+              </label>
+              <input
+                type="text"
+                placeholder="Merchant"
+                className="mb-3 w-full rounded-lg border-[1.5px] border-stroke bg-transparent bg-white px-5 py-3 outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div>
+              <CountrySelect />
+            </div>
+            <label className="  mb-3 mt-3 block border-t-2  pt-2 text-sm font-medium dark:text-white">
+              Select Merchant
+            </label>
+            <div className="">
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Page;
---
+++ src/app/uploadFile/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import UploadFile from "@/components/payout/UploadFile";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+const Page = () => {
+  return (
+    <>
+      <DefaultLayout>
+        <UploadFile />
+      </DefaultLayout>
+    </>
+  );
+};
+
+export default Page;
